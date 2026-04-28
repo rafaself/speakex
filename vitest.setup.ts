8ead9ef97eba2d78720fadb3ca60cdcd0f8a555c
rafael <rafaondjango@gmail.com>
2026-04-28 01:04:35 -0300

    style: Polyfill Element.animate for compatibility in tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,6 +3,27 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/svelte";
 import { afterEach } from "vitest";
 
+if (!Element.prototype.animate) {
+  Object.defineProperty(Element.prototype, "animate", {
+    writable: true,
+    value: () => ({
+      cancel() {},
+      commitStyles() {},
+      finished: Promise.resolve(),
+      finish() {},
+      pause() {},
+      play() {},
+      reverse() {},
+      updatePlaybackRate() {},
+      addEventListener() {},
+      removeEventListener() {},
+      dispatchEvent() {
+        return true;
+      }
+    })
+  });
+}
+
 afterEach(() => {
   cleanup();
 });
